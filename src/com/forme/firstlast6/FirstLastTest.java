cefb0efb2ed6618339838de91f0295473250cecb
irakli1990 <iraklikardawa@gmail.com>
2020-12-09 19:22:13 +0100

    first chalange done

--- src/com/forme/firstlast6/FirstLastTest.java
+++ src/com/forme/firstlast6/FirstLastTest.java
@@ -1,4 +1,19 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.forme.firstlast6;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static com.forme.firstlast6.Main.firstLast6;
+
 class FirstLastTest {
-  
+
+    @Test
+    void firstLast6Test() {
+        int[] nums = new int[]{1, 2, 6};
+        int[] nums1 = new int[]{6, 1, 2, 3};
+        int[] nums2 = new int[]{13, 6, 1, 2, 3};
+        Assertions.assertTrue(firstLast6(nums));
+        Assertions.assertTrue(firstLast6(nums1));
+        Assertions.assertFalse(firstLast6(nums2));
+    }
 }
--- src/com/forme/firstlast6/Main.java
+++ src/com/forme/firstlast6/Main.java
@@ -1,6 +1,14 @@
 package com.forme.firstlast6;
 
 
+/**
+ * Given an array of ints, return true if 6 appears as either the first or last element in the array.
+ * The array will be length 1 or more.
+ * firstLast6([1, 2, 6]) → true
+ * firstLast6([6, 1, 2, 3]) → true
+ * firstLast6([13, 6, 1, 2, 3]) → false
+ */
+
 public class Main {
 
     public static void main(String[] args) {
@@ -12,13 +20,6 @@ public class Main {
         System.out.println(firstLast6(nums2));
     }
 
-    /**
-     * Given an array of ints, return true if 6 appears as either the first or last element in the array.
-     * The array will be length 1 or more.
-     * firstLast6([1, 2, 6]) → true
-     * firstLast6([6, 1, 2, 3]) → true
-     * firstLast6([13, 6, 1, 2, 3]) → false
-     */
     public static boolean firstLast6(int[] nums) {
         if (nums[0] == 6) {
             return true;
