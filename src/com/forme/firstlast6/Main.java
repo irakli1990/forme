baa7909ec6535ab7edd99c5b8bd3e42d34a0304a
irakli1990 <iraklikardawa@gmail.com>
2020-12-09 19:21:32 +0100

    idea folder added to git ignore

---
+++ src/com/forme/firstlast6/Main.java
@@ -0,0 +1,28 @@
+package com.forme.firstlast6;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 6};
+        int[] nums1 = new int[]{6, 1, 2, 3};
+        int[] nums2 = new int[]{13, 6, 1, 2, 3};
+        System.out.println(firstLast6(nums));
+        System.out.println(firstLast6(nums1));
+        System.out.println(firstLast6(nums2));
+    }
+
+    /**
+     * Given an array of ints, return true if 6 appears as either the first or last element in the array.
+     * The array will be length 1 or more.
+     * firstLast6([1, 2, 6]) → true
+     * firstLast6([6, 1, 2, 3]) → true
+     * firstLast6([13, 6, 1, 2, 3]) → false
+     */
+    public static boolean firstLast6(int[] nums) {
+        if (nums[0] == 6) {
+            return true;
+        } else return nums[nums.length - 1] == 6;
+    }
+
+}
