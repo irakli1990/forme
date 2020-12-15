53d1e0d8fd584cd06971bb2ad911aaea2df65450
irakli1990 <iraklikardawa@gmail.com>
2020-12-15 20:10:14 +0100

    chalange done

--- src/com/forme/multyplyby2/Dubling.java
+++ src/com/forme/multyplyby2/Dubling.java
@@ -1,4 +1,25 @@
+package com.forme.multyplyby2;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+
+/**
+ * doubling([1, 2, 3]) → [2, 4, 6]
+ * doubling([6, 8, 6, 8, -1]) → [12, 16, 12, 16, -2]
+ * doubling([]) → []
+ */
 class Dubling {
-  
+
+    @Test
+    void doubling() {
+        assertEquals (new ArrayList<Integer>(Arrays.asList(2,4,6)), Main.doubling(new ArrayList<Integer>(Arrays.asList(1,2,3))));
+        assertEquals (new ArrayList<Integer>(Arrays.asList()), Main.doubling(new ArrayList<Integer>(Arrays.asList())));
+        assertEquals (new ArrayList<Integer>(Arrays.asList(2,2,2)), Main.doubling(new ArrayList<Integer>(Arrays.asList(1,1,1))));
+    }
 }
--- src/com/forme/multyplyby2/Main.java
+++ src/com/forme/multyplyby2/Main.java
@@ -1,2 +1,18 @@
-package com.forme.multyplyby2;public class Main {
+package com.forme.multyplyby2;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static List<Integer> doubling(List<Integer> nums) {
+        List<Integer> temp = new ArrayList<Integer>();
+        if (nums.size() > 0) {
+            for (Integer num : nums) {
+                temp.add(num * 2);
+            }
+        }
+        return temp;
+    }
+
 }
